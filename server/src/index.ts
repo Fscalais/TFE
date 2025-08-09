83a11d4973d83874494413da4684c7268304b379
Fscalais <127421223+Fscalais@users.noreply.github.com>
2025-08-09 20:09:22 +0200

    Correction pour déploiement

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use(cors({
   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
 }));
-app.options('*', cors());
+app.options('(.*)', cors());
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin && [CLIENT_URL, 'http://localhost:3000'].includes(origin)) {
